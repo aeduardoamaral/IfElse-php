d0f52e76e465e6c7896168423a7453f7f576cbfa
Antonio Nascimento <aeduardoamaral@gmail.com>
2020-01-12 19:30:35 -0300

    Add files via upload

---
+++ If Else.php
@@ -0,0 +1,33 @@
+<?php
+
+
+$n=9;
+
+$m=9;
+
+
+if ($n>$m)
+        {
+
+    echo "<font color=blue>O primeiro e maior que o segundo";
+        
+        }
+
+else if($n==$m)
+        {
+
+    echo"<font color=grenn>Os numeros são iguais";
+        
+        }
+
+else
+        {
+
+    echo "<font color=red>O segundo é maior que o primeiro";
+        
+        }
+
+
+
+
+?>
